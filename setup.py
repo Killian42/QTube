080824095066eba18b65e905622e79ad1d5860bf
Killian42 <killian.lebreton35@gmail.com>
2024-03-22 10:34:35 +0100

    added toml file

--- setup.py
+++ setup.py
@@ -10,9 +10,13 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="MIT",
-    py_modules=["utils"],
-    package_dir={"": "QTube"},
     author="Killian Lebreton",
     author_email="killian.lebreton35@gmail.com",
     url="https://github.com/Killian42/QTube",
+    project_urls={
+        "Source": "https://github.com/Killian42/QTube"
+    },
+
+    py_modules=["utils"],
+    package_dir={"": "QTube"},
 )
