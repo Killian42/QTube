ac919dc9b0b65ecafe2e10307660c4694817499f
Killian42 <killian.lebreton35@gmail.com>
2024-10-20 22:06:04 +0200

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="QTube",
-    version="2.2.0",
+    version="2.3.0",
     description="Automatically add Youtube videos to a playlist.",
     long_description=long_description,
     long_description_content_type="text/markdown",
